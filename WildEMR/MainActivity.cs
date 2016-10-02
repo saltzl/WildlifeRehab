1995ee0b25722e3806988a632e7ffc5ece293bb5
Tyler Brown <tylersterlingbrown@gmail.com>
2016-10-02 16:58:08 -0400

    initial commit

---
+++ WildEMR/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace WildEMR
+{
+    [Activity(Label = "WildEMR", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button new_patient_btn; 
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            SetContentView(Resource.Layout.Main);
+
+            base.OnCreate(bundle);
+            new_patient_btn = FindViewById<Button>(Resource.Id.new_patient_btn);
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+            new_patient_btn.Click += delegate
+            {
+                StartActivity(typeof(NewPatientScreen));
+            };
+        }
+    }
+}
+
