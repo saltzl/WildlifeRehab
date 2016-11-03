8bb356859e8c496112812d42bc73e69898897b12
avsyug <gavsyuk413@gmail.com>
2016-11-03 13:21:51 -0400

    Updated New Patient code including Existing Patient screen.

--- WildEMR/NewPatientScreen.cs
+++ WildEMR/NewPatientScreen.cs
@@ -21,6 +21,7 @@ namespace WildEMR
         Spinner species_spinner;
         EditText id_text;
         string[] species_array = { " ", "Raccoon", "Fox", "Cat" };
+        Patient new_patient = new Patient();
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -28,24 +29,30 @@ namespace WildEMR
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.NewPatientScreen);
 
+            //Get user input for patient ID and species
             ArrayAdapter adapter = new ArrayAdapter(this, global::Android.Resource.Layout.SimpleSpinnerItem, species_array);
             species_spinner = FindViewById<Spinner>(Resource.Id.species_spinner);
             species_spinner.Adapter = adapter;
             id_text = (EditText)FindViewById(Resource.Id.edit_id);
 
+            //Next button handler
             next_btn = FindViewById<Button>(Resource.Id.next_button1);
             next_btn.Click += delegate
             {
-                String id_string = id_text.Text;
-                String species_string = species_spinner.SelectedItem.ToString();
+                //Set user input to patient object
+                new_patient.Patient_id = id_text.Text;
+                new_patient.Patient_species = species_spinner.SelectedItem.ToString();
+
+                //Pass information along to the next activity
                 var new_patient_screen2 = new Intent(this, typeof(NewPatientScreen2));
                 Bundle extras = new Bundle();
-                extras.PutString("ANIMAL_ID", id_string);
-                extras.PutString("ANIMAL_SPECIES", species_string);
+                extras.PutString("Patient_ID", new_patient.Patient_id);
+                extras.PutString("Patient_SPECIES", new_patient.Patient_species);
                 new_patient_screen2.PutExtras(extras);
                 StartActivity(new_patient_screen2);
             };
 
+            //Cancel button handler, brings user back to Main screen
             cancel_btn = FindViewById<Button>(Resource.Id.cancel_button1);
             cancel_btn.Click += delegate
             {
--- WildEMR/obj/Debug/android/wildemr/wildemr/R.java
+++ WildEMR/obj/Debug/android/wildemr/wildemr/R.java
@@ -15,49 +15,84 @@ public final class R {
     }
     public static final class id {
         public static final int add_photo_button=0x7f050005;
+        public static final int additional_notes_button_layout=0x7f05002e;
         public static final int additional_notes_text=0x7f050016;
         public static final int cancel_button1=0x7f05000f;
         public static final int cancel_button2=0x7f050019;
         public static final int cancel_next_layout=0x7f05000d;
         public static final int cancel_next_sublayout=0x7f05000e;
         public static final int cancel_save_layer=0x7f050018;
+        public static final int diet_button=0x7f05002d;
+        public static final int diet_button_layout=0x7f05002c;
         public static final int edit_additional_notes=0x7f050017;
         public static final int edit_height=0x7f050013;
         public static final int edit_id=0x7f05000c;
+        public static final int edit_species_text=0x7f050007;
         public static final int edit_weight=0x7f050015;
         public static final int existing_patient_btn=0x7f050002;
+        public static final int height_label=0x7f050027;
+        public static final int height_layout=0x7f050026;
         public static final int height_text=0x7f050012;
         public static final int id_text=0x7f05000b;
+        public static final int info_and_buttons_layout=0x7f05001d;
+        public static final int info_label=0x7f05001f;
+        public static final int info_layout=0x7f05001e;
         public static final int linearLayout1=0x7f050000;
+        public static final int new_button=0x7f050032;
         public static final int new_patient_btn=0x7f050001;
         public static final int new_patient_id_layout=0x7f05000a;
         public static final int new_patient_screen_2_layout=0x7f050011;
         public static final int new_patient_screen_layout=0x7f050004;
         public static final int next_button1=0x7f050010;
+        public static final int notes_button=0x7f05002f;
+        public static final int old_button=0x7f050031;
+        public static final int old_new_buttons_layout=0x7f050030;
+        public static final int patient_height=0x7f050028;
+        public static final int patient_id_label=0x7f050024;
+        public static final int patient_id_layout=0x7f050023;
+        public static final int patient_id_text=0x7f050025;
+        public static final int patient_image_view=0x7f05001c;
+        public static final int patient_profile_screen_layout=0x7f05001b;
+        public static final int patient_species_label=0x7f050021;
+        public static final int patient_species_layout=0x7f050020;
+        public static final int patient_species_text=0x7f050022;
+        public static final int patient_weight=0x7f05002b;
         public static final int save_button2=0x7f05001a;
         public static final int settings_btn=0x7f050003;
         public static final int species_button_spinner_layout=0x7f050006;
-        public static final int species_screen_spinner=0x7f05001b;
+        public static final int species_screen_spinner=0x7f050033;
         public static final int species_spinner=0x7f050009;
         public static final int species_spinner_layout=0x7f050008;
-        public static final int species_text=0x7f050007;
+        public static final int weight_label=0x7f05002a;
+        public static final int weight_layout=0x7f050029;
         public static final int weight_text=0x7f050014;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int newpatientscreen=0x7f030001;
         public static final int newpatientscreen2=0x7f030002;
-        public static final int speciesscreen=0x7f030003;
+        public static final int patientprofilescreen=0x7f030003;
+        public static final int speciesscreen=0x7f030004;
     }
     public static final class string {
         public static final int ApplicationName=0x7f040000;
         public static final int cancel_button=0x7f040002;
-        public static final int edit_additional_notes_hint=0x7f040008;
-        public static final int edit_height_hint=0x7f040006;
-        public static final int edit_id_hint=0x7f040005;
-        public static final int edit_weight_hint=0x7f040007;
+        public static final int diet_button=0x7f040006;
+        public static final int edit_additional_notes_hint=0x7f04000d;
+        public static final int edit_height_hint=0x7f04000b;
+        public static final int edit_id_hint=0x7f04000a;
+        public static final int edit_weight_hint=0x7f04000c;
+        public static final int height_label=0x7f040011;
+        public static final int id_label=0x7f04000f;
+        public static final int info_button=0x7f040005;
+        public static final int info_label=0x7f040010;
+        public static final int new_button=0x7f040009;
         public static final int next_button=0x7f040003;
+        public static final int notes_button=0x7f040007;
+        public static final int old_button=0x7f040008;
         public static final int save_button=0x7f040004;
         public static final int species_button=0x7f040001;
+        public static final int species_label=0x7f04000e;
+        public static final int weight_label=0x7f040012;
     }
 }
